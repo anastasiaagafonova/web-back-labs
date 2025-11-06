a21fe6338f50a821f794a51d757ff69bca8540f2
Анастасия Агафонова <a.anastasi2005@mail.ru>
2025-11-06 18:14:57 +0700

    правки

--- lab5.py
+++ lab5.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, redirect, render_template, request, session
+from flask import Blueprint, render_template
 
 lab5 = Blueprint('lab5', __name__)
 
+@lab5.route('/')
+def main():
+    return render_template('lab5/lab5.html')
