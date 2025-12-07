f56c8c6add8c41eab5f2899ea931818648fae941
Anastasia Agafonova <a.anastasi2005@mail.ru>
2025-12-07 16:24:44 +0700

    добавление магазина онлайн мебели

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from rgz import rgz
 from datetime import datetime
 import os
 from collections import Counter
@@ -22,6 +23,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(rgz)
 
 LOG_FILE = os.path.join(os.path.dirname(__file__), '404_log.txt')  
 
@@ -112,7 +114,7 @@ def index():
             <li><a href="/lab7">Лабораторная работа №7</a></li>
         </ul>
         <ul>
-            <li><a href="/rgz">Расчётно-графическое задание</a></li>
+            <li><a href="/rgz">Магазин мебели "Отдыхайка"</a></li>
         </ul>
     </body>
     <footer>
