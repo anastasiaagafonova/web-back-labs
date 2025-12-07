a72f958d11cf53db651675d67eb7d89c33d321d4
Anastasia Agafonova <a.anastasi2005@mail.ru>
2025-12-07 16:00:33 +0700

    Расчётно-графическое задание

---
+++ rgz.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, url_for, request, render_template, make_response, redirect, session, current_app, jsonify
+import re
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+from os import path
+
+rgz = Blueprint('rgz', __name__)
+
+@rgz.route('/rgz/')
+def lab():
+    return render_template('lab5/lab5.html', login=session.get('login'))
+
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+                host='127.0.0.1',
+                database='anastasia_agafonova_knowledge_base',
+                user='anastasia_agafonova_knowledge_base',
+                password='123'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
