77d8e59256a49a5f55b2429dc4a6de554ae3e694
Anastasia Agafonova <a.anastasi2005@mail.ru>
2025-12-12 15:23:15 +0700

    регистрация

--- lab8.py
+++ lab8.py
@@ -2,6 +2,8 @@ from flask import Blueprint, url_for, request, render_template, make_response, r
 from datetime import datetime
 import sqlite3
 from pathlib import Path
+from db import db
+from db.models import users, articles
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -9,3 +11,32 @@ lab8 = Blueprint('lab8', __name__)
 def main():
     return render_template('lab8/index.html')
 
+
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/register.html', error='Логин не может быть пустым')
+
+    if not password_form:
+        return render_template('lab8/register.html', error='Пароль не может быть пустым')
+    
+    login_exists = users.query.filter_by(login=login_form).first()
+    
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    login_user(new_user, remember=False)
+    
+    return redirect('/lab8/')
